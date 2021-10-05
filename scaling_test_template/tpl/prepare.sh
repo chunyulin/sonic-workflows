e6573b036c489384a3a195c0b22aa86ae4f7fbea
Chun-Yu Lin <lincy@nchc.org.tw>
2021-10-05 14:52:27 +0800

    Allow to control repeating times for each number of client.

--- scaling_test_template/tpl/prepare.sh
+++ scaling_test_template/tpl/prepare.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+CLIENTS=( 30 25 20 15 10 5  1 )  ## number of simultanious clients
+REPEATS=( 1  1  1  1  2  2 10 )  ## repeat times for each CLIENTS...
+THREADS=4
+
+#### ===========================================================================
 DAG="dagfile.dag"
 rm -f $DAG
 
-THREADS=4
-CLIENTS=( 45 50 55 60 70 80 90 100 110 120 130 140 )
-CLIENTS=( 280 )
 N=${#CLIENTS[@]}
 
 write_dag_job() {
-    echo "JOB $1 $1.condor DIR $1" >> $DAG
+    echo "JOB $1 $2.condor DIR $2" >> $DAG
+    echo "SCRIPT POST $1 echo" >> $DAG
 }
 write_dag_depend() {
     echo "PARENT $1 CHILD $2" >> $DAG
@@ -16,20 +19,23 @@ write_dag_depend() {
 
 
 ## prepare working folder
+J0=""
 for (( i = 0; i < N; i++ )); do
-    SUB=`printf "q%03d" ${CLIENTS[$i]}`
-    #echo $SUB
-    mkdir -p $SUB
-    sed "s/^Queue .*/Queue ${CLIENTS[$i]}/g" tpl.condor | sed "s/_THREADS_/${THREADS}/g"  > $SUB/$SUB.condor
-    
-    write_dag_job $SUB
+
+    FOLDER=`printf "q%03d" ${CLIENTS[$i]}`
+    mkdir -p $FOLDER
+
+    R=${REPEATS[$i]}
+    for (( j = 0; j < R; j++ )); do
+
+        sed "s/^Queue .*/Queue ${CLIENTS[$i]}/g" tpl.condor | sed "s/_THREADS_/${THREADS}/g"  > $FOLDER/$FOLDER.condor
+
+        JID=`printf "q%03d_%02d" ${CLIENTS[$i]} ${j}`
+        write_dag_job $JID $FOLDER
+
+        ## set dependence
+        [ ! -z "$J0" ] && write_dag_depend $J0 $JID
+        J0=$JID
+        echo  >> $DAG
+    done
 done
-
-
-for (( i = 1; i < N; i++ )); do
-    S0=`printf "q%03d" ${CLIENTS[$i-1]}`
-    S1=`printf "q%03d" ${CLIENTS[$i]}`
-    write_dag_depend $S0 $S1
-done
-
-
